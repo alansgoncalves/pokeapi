df4bf093e9d71880bf5c2c504d7317842082b8f3
alansgoncalves <vekio.soft@gmail.com>
2022-11-06 21:24:29 -0300

    style: add css style to searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,6 +3,7 @@ import { searchPokemon } from "./api";
 
 const SearchBar = () => {
   const [search, setSearch] = useState("");
+  const [pokemon, setPokemon] = useState();
 
   const handleChange = (e) => {
     setSearch(e.target.value);
@@ -10,13 +11,26 @@ const SearchBar = () => {
 
   const handleClick = async (e) => {
     const data = await searchPokemon(search);
-    console.log(data);
+    setPokemon(data);
   };
 
   return (
-    <div>
-      <input placeholder="Pesquisar pokemon..." onChange={handleChange} />
-      <button onClick={handleClick}>Pesquisar</button>
+    <div className="cont-searchbar">
+      <div className="search-input">
+        <input placeholder="Pesquisar pokemon..." onChange={handleChange} />
+      </div>
+      <div className="btn-search">
+        <button onClick={handleClick}>Pesquisar</button>
+      </div>
+      <div>
+        {pokemon && (
+          <div>
+            <div>Name: {pokemon.name}</div>
+            <div>Peso: {pokemon.weight}</div>
+            <img src={pokemon.sprites.front_default} alt="" />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
